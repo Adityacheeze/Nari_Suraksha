813cc9cc2b676894300940d4f3bd3556575c4bb8
Anshika Saxena <developer.anshika467@gmail.com>
2025-08-19 12:28:02 +0530

    Emergency Portal Initialised

---
+++ src/Emergency.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Emergency = () => {
+  return (
+    <div>
+      <h1>This is the portal for Emergency Contacts.</h1>
+    </div>
+  )
+}
+
+export default Emergency
