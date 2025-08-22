40a8f8bfd06faf19ae748f51f4d537169c594845
Anshika Saxena <developer.anshika467@gmail.com>
2025-08-22 15:02:01 +0530

    Fixing the image issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,8 @@
 import "./App.css";
-import Emergency from "./components/Emergency";
-import EmergencyAccordion from "./components/EmergencyAccordion";
-import EmergencyFAQ from "./components/EmergencyFAQ";
-import Safety from "./Safety";
 
 function App() {
   return (
     <>
-      {/* <div>
-        <h1 class="text-3xl font-bold underline">Hello World!!!</h1>
-        <h2>changes</h2>
-      </div> */}
       {/* <Emergency /> */}
       {/* <Safety/> */}
     </>
